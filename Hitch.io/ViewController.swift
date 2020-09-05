ac77c22ea4dd482258c5765b9deed0a9e119e3e1
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2020-09-05 17:17:41 +0530

    Adding location on pressing register

--- Hitch.io/ViewController.swift
+++ Hitch.io/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var emailTf: UITextField!
     
@@ -19,16 +21,51 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+//        locationManager.requestLocation()
+        
+        
+        
         emailTf.borderStyle = UITextField.BorderStyle.roundedRect
         passwordTf.borderStyle = UITextField.BorderStyle.roundedRect
         
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
-        passwordTf.endEditing(true)
+        locationManager.requestLocation()
+        
+        
     }
     
+    
+    
+    
+}
 
+//MARK: - CLLocationManager Delegate
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Location updated")
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            print(lat)
+            print(long)
+        }
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
 }
 
